3d4a5443cd014e621e22cad46482ba30de102a90
Varma1959 <105090552+Varma1959@users.noreply.github.com>
2023-05-18 11:39:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,73 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
-import { useEffect, useState } from "react";
-const url = "https://course-api.com/react-tabs-project";
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-  const [showData, setShowData] = useState("");
+  const [value, setValue] = useState(2);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(url);
-      const { data } = response
-      setJobs(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
-    fetchData();
+    const fetchJobs = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://course-api.com/react-tabs-project"
+        );
+        const { data } = response;
+        setJobs(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchJobs();
   }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
   return (
-    <div>
-      <button onClick={() => setShowData("title")}> Title</button>
-      <button onClick={() => setShowData("dates")}> Dates</button>
-      <table border="1">
-        <tr>
-          <th>Title</th>
-          <th>Dates</th>
-        </tr>
-        {jobs.map((job) => (
-          <tr>
-            <td>{showData == "title" ? job.title : ""} </td>
-            <td>{showData == "dates" ? job.dates : ""} </td>
-          </tr>
-        ))}
-      </table>
-    </div>
+    <section>
+    <h2>experience</h2>
+       
+      <div>
+        {/* btn container */}
+        <div>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}>
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* job info */}
+        <article>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button>more info</button>
+    </section>
   );
 }
+
 export default App;
